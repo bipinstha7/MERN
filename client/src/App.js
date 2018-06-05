c06c9eacd9327bfd93c13c0b5a77a5cf5ff9f131
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-05 13:33:00 +0545

    add reducer and actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
+import {Provider} from "react-redux";
+
+import store from "./store";
 
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
@@ -8,20 +11,23 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store} >
+        <BrowserRouter>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
---
+++ client/src/actions/authActions.js
@@ -0,0 +1,14 @@
+import {GET_ERRORS} from "./types";
+import axios from "axios";
+
+// Register User
+export const registerUser = userData => dispatch => {
+  axios.post("/api/users/register", userData)
+    .then(result => console.log("result.data",result.data))
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};

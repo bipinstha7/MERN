6022488f177c509daa2a52ea25b30cf3d008fa33
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-06 18:06:54 +0545

    add login atuthentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 import {Provider} from "react-redux";
+import jwt_decode from "jwt-decode";
 
+import setAuthToken from "./utils/setAuthToken";
+import {setCurrentUser} from "./actions/authActions";
 import store from "./store";
 
 import "./App.css";
@@ -11,6 +14,15 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+//check for token
+if(localStorage.jwtToken) {
+  // set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // decode token and get user info and experience
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // set user in isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {loginUser} from "../../actions/authActions";
+import classnames from "classnames";   
 
 class Login extends Component {
-  constructor() {
+  constructor() { 
     super();
     this.state = {
       email: "",
       password: "",
       errors: {}
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+
+    if(nextProps.errors) {
+      this.setState({errors: nextProps.errors});
     }
   }
 
@@ -22,10 +36,12 @@ class Login extends Component {
       password: this.state.password
     }
 
-    console.log(newLoginUser);
+    this.props.loginUser(newLoginUser);
   }
 
   render() {
+    const {errors} = this.state;
+
     return (
       <div className="login">
         <div className="container">
@@ -33,12 +49,26 @@ class Login extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
               <p className="lead text-center">Sign in to your DevConnector account</p>
-              <form onSubmit={this.onSubmit}>
+              <form onSubmit={this.onSubmit} noValidate >
                 <div className="form-group">
-                  <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange} />
+                  <input 
+                    type="email" className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.email
+                    })} 
+                    placeholder="Email Address" name="email" 
+                    value={this.state.email} 
+                    onChange={this.onChange} 
+                  />
+                  {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
                 </div>
                 <div className="form-group">
-                  <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange} />
+                  <input 
+                    type="password" className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password
+                    })} placeholder="Password" name="password" 
+                    value={this.state.password} onChange={this.onChange} 
+                  />
+                  {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
@@ -50,4 +80,15 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, {loginUser})(Login);

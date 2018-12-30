0ee51807f771ac43b05274dc4458db17dd81f8a6
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-30 19:02:06 +0545

    show experience component

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import dayjs from 'dayjs'
+
+import { deleteExperience } from '../../actions/profileActions'
+
+class Experience extends Component {
+	onDeleteClick = id => {
+		this.props.deleteExperience(id)
+	}
+
+	render() {
+		const experience = this.props.experience.map(exp => {
+			return (
+				<tr key={exp._id}>
+					<td>{exp.company}</td>
+					<td>{exp.title}</td>
+					<td>
+						{dayjs(exp.from).format('YYYY/MM/DD')} -{' '}
+						{exp.to ? dayjs(exp.to).format('YYYY/MM/DD') : 'now'}
+					</td>
+					<td>
+						<button className="btn btn-danger" onClick={() => this.onDeleteClick(exp._id)}>Delete</button>
+					</td>
+				</tr>
+			)
+		})
+
+		return (
+			<div>
+				<h4 className="mb-4">Experience Credentials</h4>
+				<table className="table">
+					<thead>
+						<tr>
+							<th>Company</th>
+							<th>Title</th>
+							<th>Years</th>
+							<th />
+						</tr>
+					</thead>
+					<tbody>{experience}</tbody>
+				</table>
+			</div>
+		)
+	}
+}
+
+Experience.propTypes = {
+	deleteExperience: PropTypes.func.isRequired
+}
+
+export default connect(
+	null,
+	{ deleteExperience }
+)(Experience)

f63904492c53b2bf7699ecfa06dd59d8519e6816
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-07 13:22:59 +0545

    wrap App.js with redux Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
@@ -8,20 +10,24 @@ import Login from './components/Auth/Login'
 import Footer from './components/layout/Footer'
 import './App.css'
 
+const store = createStore(() => [], {}, applyMiddleware)
+
 class App extends Component {
 	render() {
 		return (
-			<BrowserRouter>
-				<div className="App">
-					<Navbar />
-					<Route exact path="/" component={Landing} />
-					<div className="container">
-						<Route exact path="/register" component={Register} />
-						<Route exact path="/login" component={Login} />
+			<Provider store={store}>
+				<BrowserRouter>
+					<div className="App">
+						<Navbar />
+						<Route exact path="/" component={Landing} />
+						<div className="container">
+							<Route exact path="/register" component={Register} />
+							<Route exact path="/login" component={Login} />
+						</div>
+						<Footer />
 					</div>
-					<Footer />
-				</div>
-			</BrowserRouter>
+				</BrowserRouter>
+			</Provider>
 		)
 	}
 }

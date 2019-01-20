5d1a3c30c3fe1e0d79dc6fe3735ee7faa26192c2
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-20 20:45:44 +0545

    post action to add post data

---
+++ client/src/actions/postActions.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import { ADD_POST, GET_ERRORS } from './types'
+
+/* Add Post */
+export const addPost = postData => dispatch => {
+	axios
+		.post(`/api/posts`, postData)
+		.then(response =>
+			dispatch({
+				type: ADD_POST,
+				payload: response.data
+			})
+		)
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		)
+}

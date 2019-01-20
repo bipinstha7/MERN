66e2ab64690ca26859766ee0de2ee7d5c409cbe4
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-20 22:26:27 +0545

    post delete, like and unlike actions

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from './types'
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING, DELETE_POST } from './types'
 
 /* Add Post */
 export const addPost = postData => dispatch => {
@@ -34,6 +34,50 @@ export const getPosts = () => dispatch => {
 			dispatch({
 				type: GET_POSTS,
 				payload: {}
+			})
+		)
+}
+
+/* Delete Post */
+export const deletePost = id => dispatch => {
+	axios
+		.delete(`/api/posts/${id}`)
+		.then(result =>
+			dispatch({
+				type: DELETE_POST,
+				payload: id
+			})
+		)
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		)
+}
+
+/* Add Like */
+export const addLike = id => dispatch => {
+	axios
+		.post(`/api/posts/like/${id}`)
+		.then(result => dispatch(getPosts()))
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		)
+}
+
+/* Remove Like */
+export const removeLike = id => dispatch => {
+	axios
+		.post(`/api/posts/unlike/${id}`)
+		.then(result => dispatch(getPosts()))
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
 			})
 		)
 }

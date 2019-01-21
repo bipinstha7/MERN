bf4f555fa984251cb8b360e8e08cf3f5d9a3ebb0
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-22 00:09:11 +0545

    users router

---
+++ routes/api/users.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+/**
+ * @route   GET api/users/test
+ * @desc    Test post route
+ * @access  Public
+ */
+router.get('/test', (req, res) => {
+    res.json({message: 'user message'})
+})
+
+module.exports = router

90a705a4aa78733e46c280b0cb89f6c05f27d5dc
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-23 00:57:18 +0545

    check input validation

---
+++ validation/login.js
@@ -0,0 +1,30 @@
+const Validator = require('validator')
+const isEmpty = require('./is_empty')
+
+const validateLoginInput = data => {
+    let errors = {}
+
+    let { email, password } = data
+    
+    email = !isEmpty(email) ? email : ''
+    password = !isEmpty(password) ? password : ''
+
+    if (Validator.isEmpty(email)) {
+        errors.email = 'Email field is required'
+    }
+
+    if (!Validator.isEmail(email)) {
+        errors.email = 'Email is invalid'
+    }
+
+    if (Validator.isEmpty(password)) {
+        errors.password = 'Password field is required'
+    }
+
+	return {
+		errors, 
+		isValid: isEmpty(errors),
+	}
+}
+
+module.exports = validateLoginInput

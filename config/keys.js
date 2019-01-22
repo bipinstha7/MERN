91a3729c2bb1a9717a93d864e08abca133449ca1
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-22 23:33:08 +0545

    secret json key

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://mern:mern1dev@ds163164.mlab.com:63164/mern_dev'
+    mongoURI: 'mongodb://mern:mern1dev@ds163164.mlab.com:63164/mern_dev',
+    secretOrKey: 'Secret JsonWebToken Key'
 }

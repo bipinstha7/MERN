5fd1c072932df4051a92a02e6d2330db0e05d2fe
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-24 21:45:45 +0545

    add catch block

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -177,6 +177,9 @@ router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (r
 			.then(post => res.status(200).json(post))
 			.catch(err => res.status(500).json({ err: err.message }))
 	})
+	.catch(err => res.status(500).json({err: err.message}))
 })
+
+
 
 module.exports = router

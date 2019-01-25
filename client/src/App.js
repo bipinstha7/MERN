0c45f40859edcc54b1a110cedaeecfd837609eed
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-25 22:46:02 +0545

    import and use Navbar, Landing and Footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+import Footer from './components/layout/Footer'
 import './App.css'
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<h1>Bipin Shrestha</h1>
-			</div>
+			<BrowserRouter>
+				<div className="App">
+					<Navbar />
+					<Route exact path="/" component={Landing} />
+					<Footer />
+				</div>
+			</BrowserRouter>
 		)
 	}
 }

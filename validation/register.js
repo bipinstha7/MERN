0ab42843771549304669b9f8a58aea607226e1c9
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-26 00:04:55 +0545

    fix register validation typo/error

--- validation/register.js
+++ validation/register.js
@@ -2,50 +2,50 @@ const Validator = require('validator')
 const isEmpty = require('./is_empty')
 
 const validateRegisterInput = data => {
-    let errors = {}
+	let errors = {}
 
-    let {name, email, password, password2} = data
-    
-    name = !isEmpty(name) ? name : ''
-    email = !isEmpty(email) ? email : ''
-    password = !isEmpty(password) ? password : ''
-    password2 = !isEmpty(password2) ? password2 : ''
+	let { name, email, password, password2 } = data
+
+	name = !isEmpty(name) ? name : ''
+	email = !isEmpty(email) ? email : ''
+	password = !isEmpty(password) ? password : ''
+	password2 = !isEmpty(password2) ? password2 : ''
 
 	if (!Validator.isLength(name, { min: 2, max: 30 })) {
 		errors.name = 'Name must be between 2 and 30 characters'
-    }
+	}
 
-    if (Validator.isEmpty(name)) {
-        errors.name = 'Name field is required'
-    }
+	if (Validator.isEmpty(name)) {
+		errors.name = 'Name field is required'
+	}
 
-    if (Validator.isEmpty(email)) {
-        errors.email = 'Email field is required'
-    }
+	if (Validator.isEmpty(email)) {
+		errors.email = 'Email field is required'
+	}
 
-    if (!Validator.isEmail(email)) {
-        errors.email = 'Email is invalid'
-    }
+	if (!Validator.isEmail(email)) {
+		errors.email = 'Email is invalid'
+	}
 
-    if (Validator.isEmpty(password)) {
-        errors.password = 'Password field is required'
-    }
+	if (Validator.isEmpty(password)) {
+		errors.password = 'Password field is required'
+	}
 
-    if (!Validator.isLength(password, { min: 6, max: 30 })) {
-        errors.password = 'Password must be at least 6 characters'
-    }
+	if (!Validator.isLength(password, { min: 6, max: 30 })) {
+		errors.password = 'Password must be at least 6 characters'
+	}
 
-    if (Validator.isEmpty(password2)) {
-        errors.password2 = 'Confirm Password field is required'
-    }
+	if (Validator.isEmpty(password2)) {
+		errors.password2 = 'Confirm Password field is required'
+	}
 
-    if (!Validator.equals(password, password2)) {
-        errors.email = 'Passwords mush match'
-    }
+	if (!Validator.equals(password, password2)) {
+		errors.password = 'Passwords mush match'
+	}
 
 	return {
-		errors, 
-		isValid: isEmpty(errors),
+		errors,
+		isValid: isEmpty(errors)
 	}
 }
 

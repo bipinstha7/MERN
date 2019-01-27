ecea1ef69324825dfa4a625cd3e00955769a2919
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-27 11:12:20 +0545

    clear profile when the token expires

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,15 @@ import { Provider } from 'react-redux'
 import jwt_decode from 'jwt-decode'
 
 import { setCurrentUser, logoutUser } from './actions/authActions'
+import { clearCurrentProfile } from './actions/profileActions'
 import setAuthToken from './utils/setAuthToken'
 import store from './store'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
-import Register from './components/Auth/Register'
-import Login from './components/Auth/Login'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 import Footer from './components/layout/Footer'
+import Dashboard from './components/dashboard/Dashboard'
 import './App.css'
 
 /** During browser refresh, redux store goes to its inital state
@@ -37,6 +39,7 @@ if (localStorage.getItem('jwtToken')) {
 		store.dispatch(logoutUser())
 
 		/* clear current profile */
+		store.dispatch(clearCurrentProfile())
 
 		/* redirect to login */
 		window.location.href = '/login'
@@ -54,6 +57,7 @@ class App extends Component {
 						<div className="container">
 							<Route exact path="/register" component={Register} />
 							<Route exact path="/login" component={Login} />
+							<Route exact path="/dashboard" component={Dashboard} />
 						</div>
 						<Footer />
 					</div>

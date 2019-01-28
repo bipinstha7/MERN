05bea6817efb252eab027572c764d9b9ab50094c
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-28 23:10:35 +0545

    make sure to get logged in after browser refresh

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './store'
+import jwt_decode from 'jwt-decode'
 
+import { setCurrentUser } from './actions/authActions'
+import setAuthToken from './utils/setAuthToken'
+import store from './store'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Register from './components/Auth/Register'
 import Login from './components/Auth/Login'
 import Footer from './components/layout/Footer'
 import './App.css'
+
+/** During browser refresh, redux store goes to its inital state
+ * And the authentication is false
+ * To prevent this behaviour, check localstorage for token
+ * And based on the token set the authentication when page loads
+ */
+
+/* check for token */
+if (localStorage.getItem('jwtToken')) {
+	/* set auth token header auth */
+	setAuthToken(localStorage.getItem('jwtToken'))
+
+	/* decode token and get user info & expiration */
+	const decoded = jwt_decode(localStorage.getItem('jwtToken'))
+
+	/* set user and isAuthencated */
+	store.dispatch(setCurrentUser(decoded))
+}
 
 class App extends Component {
 	render() {

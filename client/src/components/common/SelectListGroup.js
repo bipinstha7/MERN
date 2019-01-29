43f1bfd2ef92a91d457229855cf6ab010ca847e2
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-29 22:09:12 +0545

    input select component

---
+++ client/src/components/common/SelectListGroup.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+
+const SelectListGroup = props => {
+	const { name, value, error, info, onChange, options } = props
+
+	const selectOptions = options.map(option => {
+		return (
+			<option value={option.value} key={option.label}>
+				{option.label}
+			</option>
+		)
+	})
+
+	return (
+		<div className="form-group">
+			<select
+				className={classNames('form-control form-control-lg', {
+					'is-invalid': error
+				})}
+				name={name}
+				value={value}
+				onChange={onChange}
+			>
+				{selectOptions}
+			</select>
+			{info && <small className="form-text text-muted" />}
+			{error && <div className="invalid-feedback">{error}</div>}
+		</div>
+	)
+}
+
+SelectListGroup.propTypes = {
+	name: PropTypes.string.isRequired,
+	value: PropTypes.string.isRequired,
+	error: PropTypes.string,
+	info: PropTypes.string,
+	onChange: PropTypes.func.isRequired,
+	options: PropTypes.array.isRequired
+}
+
+export default SelectListGroup

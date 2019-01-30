783a5b4b2b8606dab82c533d06e00f5e3c239142
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-30 21:32:55 +0545

    add-experience route

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import PrivateRoute from './components/common/PrivateRoute'
 import CreateProfile from './components/create-profile/CreateProfile'
 import EditProfile from './components/edit-profile/EditProfile'
 import AddExperience from './components/add-credentials/AddExperience'
+import AddEducation from './components/add-credentials/AddEducation'
 import './App.css'
 
 /** During browser refresh, redux store revert back to its inital state
@@ -66,6 +67,7 @@ class App extends Component {
 								<PrivateRoute exact path="/create-profile" component={CreateProfile} />
 								<PrivateRoute exact path="/edit-profile" component={EditProfile} />
 								<PrivateRoute exact path="/add-experience" component={AddExperience} />
+								<PrivateRoute exact path="/add-education" component={AddEducation} />
 							</Switch>
 						</div>
 						<Footer />

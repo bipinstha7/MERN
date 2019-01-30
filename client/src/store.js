d245793feb12bf045118c3a476db455f2e39521f
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-30 23:47:34 +0545

    redux dev tool setup

--- client/src/store.js
+++ client/src/store.js
@@ -7,6 +7,7 @@ const initialState = {}
 
 const middleware = [thunk]
 
+/* redux dev tool setup */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...middleware)))
 

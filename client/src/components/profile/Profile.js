abbf0acee5c7449f190c871a37fb4a7971b5de24
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-31 23:57:59 +0545

    github componet

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -17,6 +17,13 @@ class Profile extends Component {
 		}
 	}
 
+	// static getDerivedStateFromProps(nextProps, prevState) {
+	// 	if (nextProps.profile.profile === null && prevState.loading) {
+	// 		return nextProps.history.push('/not-found')
+	// 	}
+	// 	return null
+	// }
+
 	render() {
 		const { profile, loading } = this.props.profile
 
@@ -37,7 +44,8 @@ class Profile extends Component {
 					<ProfileHeader profile={profile} />
 					<ProfileAbout profile={profile} />
 					<ProfileCreds education={profile.education} experience={profile.experience} />
-					<ProfileGithub />
+
+					{profile.githubusername ? <ProfileGithub username={profile.githubusername} /> : null}
 				</div>
 			)
 		}

21276b93a0f561d42ae4d362d9d47aaeb0d82c9b
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-31 22:26:23 +0545

    get profile by hande action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -41,6 +41,25 @@ export const getCurrentProfile = () => dispatch => {
 		)
 }
 
+/* Get profile by handle */
+export const getProfileByHandle = handle => dispatch => {
+	dispatch(setProfileLoading())
+	axios
+		.get(`/api/profile/handle/${handle}`)
+		.then(profile => {
+			dispatch({
+				type: GET_PROFILE,
+				payload: profile.data
+			})
+		})
+		.catch(err =>
+			dispatch({
+				type: GET_PROFILE,
+				payload: null
+			})
+		)
+}
+
 /* Create Profile */
 export const createProfile = (profileData, history) => dispatch => {
 	axios
